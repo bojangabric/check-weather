3360a9637c4925c4d4fa003add15df9378b77f4e
bojangabric <bojan@bojangabric.com>
2019-03-18 20:15:51 +0100

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import WeatherInfo from './components/WeatherInfo';
+import { Provider } from 'react-redux';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header" />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <SearchBar />
+          <WeatherInfo />
+        </div>
+      </Provider>
     );
   }
 }
