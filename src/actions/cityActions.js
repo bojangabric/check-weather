73961a84c0ca1ff6daa4b409d0536c9c726320a7
bojangabric <bojan@bojangabric.com>
2019-03-18 20:16:20 +0100

    Add city actions and types

---
+++ src/actions/cityActions.js
@@ -0,0 +1,13 @@
+import { GET_CITY_INFO } from "./types";
+
+export const getCityInfo = (city_name) => dispatch => {
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=1c2c0597a063bf5261ed0a4a2ba10781&units=metric`)
+    .then(res => res.json())
+    .then(city_info =>
+      dispatch({
+        type: GET_CITY_INFO,
+        payload: city_info
+      })
+    )
+    .catch((e) => console.log(e))
+};
