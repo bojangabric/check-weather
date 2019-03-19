993dd7ad063c23461ba9aac0a2291b3f242021a4
bojangabric <bojan@bojangabric.com>
2019-03-19 19:21:36 +0100

    Change api to https

--- src/actions/cityActions.js
+++ src/actions/cityActions.js
@@ -1,7 +1,7 @@
 import { GET_CITY_INFO } from "./types";
 
 export const getCityInfo = (city_name) => dispatch => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=1c2c0597a063bf5261ed0a4a2ba10781&units=metric`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=1c2c0597a063bf5261ed0a4a2ba10781&units=metric`)
     .then(res => res.json())
     .then(city_info =>
       dispatch({
