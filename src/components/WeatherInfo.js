c5363bbd4b09627838d95d743271894a874c604b
bojangabric <bojan@bojangabric.com>
2019-03-20 13:32:31 +0100

    Add transitions to WeatherInfo.js

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
+import InfoRow from './InfoRow';
 import './WeatherInfo.css';
 import { connect } from 'react-redux';
+import { CSSTransition } from "react-transition-group";
 
 class WeatherInfo extends Component {
   render() {
-
     if (!this.props.city || Object.keys(this.props.city).length === 0 || !this.props.city.main) {
       return (
-        <div className='no-info-wrapper'>
-          <div>There is no information for that city.</div>
-        </div>
+        <CSSTransition
+          in={true}
+          appear={true}
+          timeout={600}
+          classNames='fade'
+        >
+          <div className='no-info-wrapper'>
+            <div>There is no information for that city.</div>
+          </div>
+        </CSSTransition>
       );
     }
 
     return (
       <div className='info-wrapper'>
-        <div><span className='info-name'>Temp:</span><span className='info-value'>{this.props.city.main.temp}&#8451;</span></div>
-        <div><span className='info-name'>Temp-max:</span><span className='info-value'>{this.props.city.main.temp_max}&#8451;</span></div>
-        <div><span className='info-name'>Temp-min:</span><span className='info-value'>{this.props.city.main.temp_min}&#8451;</span></div>
-        <div><span className='info-name'>Pressure:</span><span className='info-value'>{this.props.city.main.pressure}</span></div>
-        <div><span className='info-name'>Humidity:</span><span className='info-value'>{this.props.city.main.humidity}</span></div>
-        <div><span className='info-name'>Weather:</span><span className='info-value'>{this.props.city.weather[0].description}</span></div>
+        <InfoRow name='Temp' value={this.props.city.main.temp} symbol='&#8451;' />
+        <InfoRow name='Temp-max' value={this.props.city.main.temp_max} symbol='&#8451;' />
+        <InfoRow name='Temp-min' value={this.props.city.main.temp_min} symbol='&#8451;' />
+        <InfoRow name='Pressure' value={this.props.city.main.pressure} symbol=' hPa' />
+        <InfoRow name='Humidity' value={this.props.city.main.humidity} symbol='%' />
+        <InfoRow name='Wind' value={this.props.city.wind.speed} symbol='m/s' />
+        <InfoRow name='Weather' value={this.props.city.weather[0].description} symbol='' />
       </div>
     );
   }
