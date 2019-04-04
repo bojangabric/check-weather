1dcbaef6bf01df7b77d5453d04a14f1f92e6971a
bojangabric <bojan@bojangabric.com>
2019-04-04 13:51:12 +0200

    Hide APPID using dotenv

--- src/actions/cityActions.js
+++ src/actions/cityActions.js
@@ -1,7 +1,8 @@
 import { GET_CITY_INFO } from "./types";
-
+require('dotenv').config({ path: '../.env' });
+  
 export const getCityInfo = (city_name) => dispatch => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=1c2c0597a063bf5261ed0a4a2ba10781&units=metric`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&APPID=${process.env.APPID}&units=metric`)
     .then(res => res.json())
     .then(city_info =>
       dispatch({
